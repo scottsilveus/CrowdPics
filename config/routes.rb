e0961c0bfdd124dd5994beee8647eb6282196197
scottsilveus <scottsilveus@gmail.com>
2014-07-07 09:40:41 -0500

    changed event code to be 7 chars, started adding routes for api v1, added inflection for API

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  # API ROUTING INFORMATION
+  namespace :api, path: '/', constraint: { subdomain: 'api' } do
+    namespace :v1 do
+      resources :api
+    end
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
