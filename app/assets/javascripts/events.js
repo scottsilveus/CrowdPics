8f333b265d675449bbdbf36c4332de31c07d7abd
aalba005 <aalba005@fiu.edu>
2014-07-08 18:56:27 -0500

    log in jquery and links

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-	$('#event_start_date').datepicker({
-    defaultDate: "+1w",
-    changeMonth: true,
-    numberOfMonths: 3,
-    onClose: function( selectedDate ) {
-      $( "#to" ).datepicker( "option", "minDate", selectedDate );
-    }
-  });
-  $( "#event_end_date" ).datepicker({
-    defaultDate: "+1w",
-    changeMonth: true,
-    numberOfMonths: 3,
-    onClose: function( selectedDate ) {
-      $( "#from" ).datepicker( "option", "maxDate", selectedDate );
-    }
-  });
-})
+// $(document).ready(function(){
+// 	$('#event_start_date').datepicker({
+//     defaultDate: "+1w",
+//     changeMonth: true,
+//     numberOfMonths: 3,
+//     onClose: function( selectedDate ) {
+//       $( "#to" ).datepicker( "option", "minDate", selectedDate );
+//     }
+//   });
+//   $("#event_end_date").datepicker({
+//     defaultDate: "+1w",
+//     changeMonth: true,
+//     numberOfMonths: 3,
+//     onClose: function( selectedDate ) {
+//       $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+//     }
+//   });
+// })
