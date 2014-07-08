6e3397741e0922bc587a628c7074a1da115993b2
aalba005 <aalba005@fiu.edu>
2014-07-08 16:26:55 -0500

    login and signup jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,19 +19,19 @@
 
 
 
-jQuery(document).ready(function($) {
-  $( "#signup" ).click(function(event) {
-    event.preventDefault();
-  $( "#signupdiv" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
+// jQuery(document).ready(function($) {
+//   $( "#signup" ).click(function(event) {
+//     event.preventDefault();
+//   $( "#signupdiv" ).toggle( "slow", function() {
+//     // Animation complete.
+//   });
+// });
 
-   $( "#signin" ).click(function(event) {
-    event.preventDefault();
-  $( "#signindiv" ).toggle( "slow", function() {
-    // Animation complete.
-  });
-});
+//    $( "#signin" ).click(function(event) {
+//     event.preventDefault();
+//   $( "#signindiv" ).toggle( "slow", function() {
+//     // Animation complete.
+//   });
+// });
 
-}); //document.ready
+// }); //document.ready
