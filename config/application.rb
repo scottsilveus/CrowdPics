218c95250518cd9dd9cbdc5f59aa147400858020
Scott Silveus <scottsilveus@gmail.com>
2014-07-09 16:10:35 -0500

    heroku

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module CrowdPics2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.exceptions_app = self.routes
-    config.assets.initialze_on_precompile = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
