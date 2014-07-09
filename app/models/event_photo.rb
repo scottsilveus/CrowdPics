c52bd6a321c3664979daae16b012c159835eb847
scottsilveus <scottsilveus@gmail.com>
2014-07-09 10:06:14 -0500

    masonry working

--- app/models/event_photo.rb
+++ app/models/event_photo.rb
@@ -5,8 +5,7 @@ class EventPhoto < ActiveRecord::Base
     attachment_size: { less_than: 5.megabytes }
 
   has_attached_file :avatar, styles: {
-    medium: '300x300>',
-    large: '400x400>',
-    i4: '500x500>',
+    medium: '200x200>',
+    large: '300x300>'
   }
 end
